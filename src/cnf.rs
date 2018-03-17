6d7bb9cd40068168b2dbbfc117661b2f9c97ed54
shinkwhek <klklpshin@gmail.com>
2018-03-17 16:00:23 +0900

    [fix] cnf data structure (del: struct Clause)

--- src/cnf.rs
+++ src/cnf.rs
@@ -1,13 +1,7 @@
 #[derive(PartialEq, Clone, Debug)]
-pub enum LiteralStatus {
-    Pure,
-    UnPure,
-}
-
-#[derive(PartialEq, Clone, Debug)]
 pub struct Literal {
-    id: usize,
-    status: LiteralStatus,
+    pub id: usize,
+    pub status: bool,
 }
 
 impl Literal {
@@ -15,12 +9,12 @@ impl Literal {
         if n > 0 {
             Literal {
                 id: n as usize,
-                status: LiteralStatus::Pure,
+                status: true,
             }
         } else if 0 > n {
             Literal {
                 id: -n as usize,
-                status: LiteralStatus::UnPure,
+                status: false,
             }
         } else {
             println!("error: {}", "don't use '0'");
@@ -29,40 +23,30 @@ impl Literal {
     }
 
     pub fn is_pure(&self) -> bool {
-        self.status == LiteralStatus::Pure
+        self.status
     }
 
-    pub fn get_id(&self) -> usize {
-        self.id
-    }
-}
-
-#[derive(PartialEq, Clone, Debug)]
-pub struct Clause {
-    body: Vec<Literal>,
-}
-
-impl Clause {
-    pub fn new(ns: &Vec<isize>) -> Clause {
-        Clause {
-            body: ns.iter()
-                .map(|n| Literal::new(*n))
-                .collect::<Vec<Literal>>(),
+    pub fn get_rev_literal(&self) -> Literal {
+        Literal {
+            id: self.id,
+            status: !self.status,
         }
     }
+}
 
-    pub fn is_empty(&self) -> bool {
-        self.body.is_empty()
-    }
-
-    pub fn is_unit(&self) -> bool {
-        self.body.len() == 1
+pub struct Clause {}
+impl Clause {
+    pub fn new(ns: &Vec<isize>) -> Vec<Literal> {
+        ns.iter()
+            .map(|n| Literal::new(*n))
+            .collect::<Vec<Literal>>()
+            .clone()
     }
 }
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct Cnf {
-    body: Vec<Clause>,
+    pub body: Vec<Vec<Literal>>,
 }
 
 impl Cnf {
@@ -70,46 +54,12 @@ impl Cnf {
         Cnf {
             body: nss.iter()
                 .map(|ns| Clause::new(ns))
-                .collect::<Vec<Clause>>(),
+                .collect::<Vec<Vec<Literal>>>(),
         }
-    }
-
-    pub fn get_body(&self) -> &Vec<Clause> {
-        &self.body
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn pure_literal() {
-        let pure_literal = Literal::new(1);
-        assert!(pure_literal.is_pure());
-    }
-
-    #[test]
-    #[should_panic]
-    fn unpure_literal() {
-        let unpure_literal = Literal::new(-1);
-        assert!(unpure_literal.is_pure());
-    }
-    #[test]
-    fn empty_clause() {
-        let a = Clause::new(&vec![]);
-        assert!(a.is_empty());
-    }
-
-    #[test]
-    fn unit_clause() {
-        let a = Clause::new(&vec![1]);
-        assert!(a.is_unit());
-    }
-
-    #[test]
-    fn show_cnf_example() {
-        let a = vec![vec![1, -2, 4], vec![-1, -3], vec![3], vec![2, 3, 4]];
-        println!("{:?}", Cnf::new(a));
-    }
 }
